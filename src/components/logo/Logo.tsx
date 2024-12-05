34282632988515a20c5d7a207b937a42d1a00021
coa3 <coa3@mail.ru>
2024-12-05 15:15:01 +0300

    logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+
+export const Logo = () => {
+    return (
+
+        <a href="">
+            <Icon iconId={'pavan007Color'}/>
+        </a>
+
+    );
+};
+
